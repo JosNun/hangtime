858384665aad8dd0c81ae2008fa6da6d7378ce50
JosNun <josiah.nunemaker@gmail.com>
2021-12-04 15:29:56 -0500

    Initial commit

---
+++ src/components/Counter.svelte
@@ -0,0 +1,19 @@
+<script>
+  let count = 0;
+
+  function add() {
+    count += 1;
+  }
+
+  function subtract() {
+    count -= 1;
+  }
+</script>
+
+<div>
+  <button on:click={subtract}> - </button>
+
+  <pre>{count}</pre>
+
+  <button on:click={add}> + </button>
+</div>
