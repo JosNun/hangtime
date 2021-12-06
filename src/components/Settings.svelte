5ace47031aa6381dfdd0f9375d6919cfd8d8d504
JosNun <josiah.nunemaker@gmail.com>
2021-12-05 19:06:02 -0500

    Add settings

---
+++ src/components/Settings.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  let isOpen = true;
+</script>
+
+<div>
+  <button
+    class="fixed left-4 top-3 text-gray-400"
+    on:click={() => (isOpen = true)}
+  >
+    Settings
+  </button>
+  <div
+    class="fixed left-0 top-0 w-96 h-full bg-gray-200 trnasform translate transition {!isOpen
+      ? '-translate-x-full'
+      : ''}"
+  >
+    <div class="flex justify-between items-center px-4 py-3">
+      <div class="font-bold text-gray-500 uppercase text-sm tracking-wide">
+        Settings
+      </div>
+
+      <button class="text-gray-600" on:click={() => (isOpen = false)}>X</button>
+    </div>
+  </div>
+</div>
