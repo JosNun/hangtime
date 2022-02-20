7311f678656e9d13c6d2c53d4557fa3170326893
JosNun <josiah.nunemaker@gmail.com>
2022-02-20 17:11:07 -0500

    Add button to toggle fullscreen

---
+++ src/components/Fullscreen.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import ArrowsExpand from "./icons/ArrowsExpand.svelte";
+
+  const toggleFullscreen = () => {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      document.body.requestFullscreen({
+        navigationUI: "hide",
+      });
+    }
+  };
+</script>
+
+<button
+  class="fixed top-3 right-4 text-gray-400 cursor-pointer"
+  on:click={() => toggleFullscreen()}
+>
+  <ArrowsExpand />
+</button>
